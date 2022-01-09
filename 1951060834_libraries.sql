d49122d1f7715107558661ce78769a6dc5524206
36-TranHaiLong-1951060834 <tranhailong2t11@gmail.com>
2022-01-09 12:12:15 +0700

    nop them csdl

---
+++ 1951060834_libraries.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 09, 2022 lúc 06:02 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `1951060834_libraries`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `docgia`
+--
+
+CREATE TABLE `docgia` (
+  `madg` int(10) NOT NULL,
+  `hovaten` varchar(50) NOT NULL,
+  `gioitinh` tinyint(1) NOT NULL,
+  `namsinh` date NOT NULL,
+  `nghenghiep` varchar(50) NOT NULL,
+  `ngaycapthe` date NOT NULL,
+  `ngayhethan` date NOT NULL,
+  `diachi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `docgia`
+--
+
+INSERT INTO `docgia` (`madg`, `hovaten`, `gioitinh`, `namsinh`, `nghenghiep`, `ngaycapthe`, `ngayhethan`, `diachi`) VALUES
+(123, 'long', 1, '0000-00-00', 'sinh vien', '0000-00-00', '0000-00-00', '');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `docgia`
+--
+ALTER TABLE `docgia`
+  ADD PRIMARY KEY (`madg`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `docgia`
+--
+ALTER TABLE `docgia`
+  MODIFY `madg` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
